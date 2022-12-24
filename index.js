eebfd645c1cf9a214ffe08232697b916c97309ed
Dev <jaiswaldevraj337@gmail.com>
2022-12-25 01:32:55 +0530

    second commit navbar, home banner, footer completed

---
+++ index.js
@@ -0,0 +1,17 @@
+// SwiperJs js initialization for home banner
+   var swiper = new Swiper(".bannerSlider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
