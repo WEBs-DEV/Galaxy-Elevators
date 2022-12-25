a891a6ee52b2bf50c2615f67139b421991f50612
Dev <jaiswaldevraj337@gmail.com>
2022-12-25 20:26:32 +0530

    home page almost completed

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// Aos initialization for animation on loading
+
+  AOS.init(
+    {
+      offset: 100
+    }
+  );
+
+
 // SwiperJs js initialization for home banner
    var swiper = new Swiper(".bannerSlider", {
     spaceBetween: 30,
@@ -13,5 +22,21 @@
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+// SwiperJs js initialization for Our clients slider
+  var swiper = new Swiper(".OurClientsSlider", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    centeredSlides: false,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
     },
   });
